7b22403673baedef5ea861776929f02754fe5c28
sun <sun@codebrick.co>
2022-08-06 01:01:48 +0900

    Define FieldOfView

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -1,18 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class FieldOfView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public float radius;
+    [SerializeField] public float angle;
+    [SerializeField] private LayerMask targetMask;
+    [SerializeField] private LayerMask obstructionMask;
+
+    private Animator animator;
+    
+    public bool canSeePlayer;
+
+    private void Start()
     {
+        animator = gameObject.GetComponent<Animator>();
         
+        StartCoroutine(FOVRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator FOVRoutine()
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(0.25f);
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (canSeePlayer)
+        {
+            var firePosition = Random.Range(0, 3); // TODO 
+            animator.SetInteger("IsFire", 1);
+        }
+        else
+        {
+            animator.SetInteger("IsFire", 0);
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            var target = rangeChecks[0].transform;
+            var directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                var distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                {
+                    canSeePlayer = true;
+                }
+                else
+                {
+                    canSeePlayer = false;
+                }
+            }
+            else
+            {
+                canSeePlayer = false;
+            }
+        }
+        else if (canSeePlayer)
+        {
+            canSeePlayer = false;
+        }
     }
 }
